af456402792123778f16f5c15260ae87716567f6
Uncle Charlie <lixiangdong@haoqix.com>
2017-02-08 22:01:40 +0800

    for special edition sheet

--- index.ios.js
+++ index.ios.js
@@ -22,6 +22,7 @@ export default class Application extends Component {
 
     this.state = {
       actionListSheetVisible: false,
+      specialEditedVisible: false,    // the special edition one 
     }
 
     this._showActionSheet = this._showActionSheet.bind(this)
@@ -29,25 +30,30 @@ export default class Application extends Component {
     this._handleCancel = this._handleCancel.bind(this)
   }
 
-  _showActionSheet() {
-    this.setState({ actionListSheetVisible: true, })
-    this.actionListSheet = this.refs.actionListSheet
+  _showActionSheet(title) {
+    let sheetType = title.toLowerCase()
+    if (sheetType === 'default') {
+      this.setState({ actionListSheetVisible: true, })
+      this.actionListSheet = this.refs.actionListSheet
 
-    let options = [
-      {
-        title: 'title 1', subTitle: 'sub title 1'
-      }, {
-        title: 'title 2', subTitle: 'sub title 2'
-      }
-    ]
-    this.actionListSheet.showWithOptions({
-      options,
-      title: 'Demo',
-    }, (buttonIndex) => {
-      console.log('=====> selected a index', buttonIndex)
-    }, () => {
-      this.setState({ actionListSheetVisible: false, })
-    })
+      let options = [
+        {
+          title: 'title 1', subTitle: 'sub title 1'
+        }, {
+          title: 'title 2', subTitle: 'sub title 2'
+        }
+      ]
+      this.actionListSheet.showWithOptions({
+        options,
+        title: 'Demo',
+      }, (buttonIndex) => {
+        console.log('=====> selected a index', buttonIndex)
+      }, () => {
+        this.setState({ actionListSheetVisible: false, })
+      })
+    } else {
+
+    }
   }
 
   /**
@@ -61,7 +67,7 @@ export default class Application extends Component {
         }
       }}>
         <View style={{ flexDirection: 'row', height: 60, flex: 1, alignItems: 'center', justifyContent: 'flex-start', }}>
-          <View style={{marginHorizontal: 15,}}>
+          <View style={{ marginHorizontal: 15, }}>
             <Text style={{ fontSize: 15, color: '#333' }}>{`分${data.terms}期`}</Text>
             <Text style={{ fontSize: 13, color: '#999999', marginTop: 9, }}>
               {data.interest_rate === 0 ? '免服务费' : `每期服务费${data.interest_rate}%`}
@@ -86,21 +92,35 @@ export default class Application extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this._showActionSheet}>
-          <View style={styles.buttonContainer}>
-            <Text style={styles.buttonText}>Pop up</Text>
-          </View>
-        </TouchableOpacity>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', }}>
+          <Button title="Default" handleClick={this._showActionSheet} />
+          <Button title="Decorated" handleClick={this._showActionSheet} />
+        </View>
         <ActionListSheet
-          useDefaultTitle={false}
+          useDefaultTitle={true}
           renderListRow={this._renderListRow}
           ref='actionListSheet'
           onCancel={this._handleCancel}
           isVisible={this.state.actionListSheetVisible} />
+        <ActionListSheet
+          useDefaultTitle={false}
+          renderListRow={this._renderListRow}
+          titleRender={this._titleRender}
+          ref='actionListSheet'
+          onCancel={this._handleCancel}
+          isVisible={this.state.actionListSheetVisible} />
       </View>
-    );
+    )
   }
 }
+
+const Button = ({title, handleClick}) => (
+  <TouchableOpacity onPress={() => handleClick(title)} style={{ marginLeft: 10, }}>
+    <View style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>{title || 'POP UP'}</Text>
+    </View>
+  </TouchableOpacity>
+)
 
 const styles = StyleSheet.create({
   container: {
